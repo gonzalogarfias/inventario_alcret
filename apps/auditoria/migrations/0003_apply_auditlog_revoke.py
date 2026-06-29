d474666b34583dc5402dba821c7d1a05c03cb5e7
gonzalogarfias <garfgonzalo@gmail.com>
2026-06-29 10:29:00 -0600

    fix: migration 0003 revoke no falla si falta role app_user

--- apps/auditoria/migrations/0003_apply_auditlog_revoke.py
+++ apps/auditoria/migrations/0003_apply_auditlog_revoke.py
@@ -1,9 +1,13 @@
 from django.db import migrations
+from django.db.utils import ProgrammingError
 
 
 def apply_revoke(apps, schema_editor):
     if schema_editor.connection.vendor == "postgresql":
-        schema_editor.execute("REVOKE UPDATE, DELETE ON auditoria_auditlog FROM app_user;")
+        try:
+            schema_editor.execute("REVOKE UPDATE, DELETE ON auditoria_auditlog FROM app_user;")
+        except ProgrammingError:
+            pass
 
 
 class Migration(migrations.Migration):
